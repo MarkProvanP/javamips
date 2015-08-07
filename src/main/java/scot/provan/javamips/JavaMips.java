6e8f127707d5c4f7183cb455666dfdbe3b48f85e
Mark Provan <markprovanp@gmail.com>
2015-08-07 14:23:45 +0100

    Add JavaMips main class

---
+++ src/main/java/scot/provan/javamips/JavaMips.java
@@ -0,0 +1,10 @@
+package scot.provan.javamips;
+
+/**
+ * Created by Mark on 07/08/2015.
+ */
+public class JavaMips {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
